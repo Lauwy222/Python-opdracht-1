ed6436a448f06983e1fc1f0d5d021bb33dfbdf1d
Laurens van der Drift <Lauwy222@hotmail.com>
2024-05-14 12:09:54 +0200

    Final

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,34 +1,50 @@
-class werknemer():
-    def __init__(self, functie, salaris, uren):
-        self.functie = functie
-        self.salaris = salaris
-        self.uren = uren
+class Werknemer():
+    def __init__(self, WerknemerNr, Loon):
+        self.WerknemerNr = WerknemerNr
+        self.Loon = Loon
+        self.verdiend = 0.0
 
+    def geefnummer(self):
+        return self.WerknemerNr
 
-class Freelancer(werknemer):
+    def geefverdiensten(self):
+        return self.verdiend
+
+    def printverdiend(self):
+        print(f'werknemer: {self.geefnummer()} verdient: {self.geefverdiensten(): 8.2f} Euro')
+
+class Freelancer(Werknemer):
+    def __init__(self, WerknemerNr, Loon):
+        Werknemer.__init__(self, WerknemerNr, Loon)
     def werkuren(self, uren):
-        self.uren = uren
+        self.verdiend = self.Loon * uren
 
-    def printverdiend(self):
-        pass
+class VasteKracht(Werknemer):
+    def __init__(self, WerknemerNr, Loon):
+        Werknemer.__init__(self, WerknemerNr, Loon)
+        self.omzetten()
 
+    def omzetten(self):
+        self.verdiend = self.Loon
 
-class VasteKracht(werknemer):
-    def printverdiend(self):
-        pass
+class Stukwerker(Werknemer):
+    def __init__(self, WerknemerNr, Loon):
+        Werknemer.__init__(self, WerknemerNr, Loon)
+    def produceerstuks(self, stuks):
+        self.verdiend = self.Loon * stuks
 
-
-f = Freelancer(1, 25.75)
-v = VasteKracht(2, 1873.53)
-
+f = Freelancer(1, 25.75)  # werknemer 1 verdient 25.75 per uur
+v = VasteKracht(2, 1873.53)  # werknemer 2 verdient (deze maand) 84 uur
+s = Stukwerker(3, 1.05)
 f.werkuren(84)
-
+s.produceerstuks(1687)
 print('Maand 1:')
 f.printverdiend()
 v.printverdiend()
-
+s.printverdiend()
 f.werkuren(13.5)
-
+s.produceerstuks(0)
 print('Maand 2:')
 f.printverdiend()
 v.printverdiend()
+s.printverdiend()
